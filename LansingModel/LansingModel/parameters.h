e9ecb6629f4bc54fcff8ff09e160f49ad2a6e657
WFOudijk <w.f.oudijk@student.rug.nl>
2023-01-23 16:51:04 +0100

    update parameters.h

--- LansingModel/LansingModel/parameters.h
+++ LansingModel/LansingModel/parameters.h
@@ -9,13 +9,13 @@
 
 struct Parameters {
     // initialise the parameters
-    Parameters() : populationSize(1000), // 500, 1500, 2000 > parameter exploration
+    Parameters() : populationSize(10000), // 500, 1500, 2000 > parameter exploration
                    initDamageProportion(0.5),
                    numOfOffspringPerFemale(2),
                    mutationProb(0.01), // 0.01, 0.15, steps: 0.02. > parameter exploration
                    extrinsicMortRisk(0.01),
-                   outputTime(5),
-                   tEnd(1000), // 10.000
+                   outputTime(10),
+                   tEnd(10000), // 10.000
                    strengthOfSelection(-0.05),
                    maximumAge(40){
                        numOfGametes = maximumAge * numOfOffspringPerFemale;
