d1d459593000102a5035e6c53ddf18aff1987cc5
WFOudijk <w.f.oudijk@student.rug.nl>
2023-02-10 14:39:35 +0100

    sex specific effects implemented

--- LansingModel/LansingModel/outputGenerator.h
+++ LansingModel/LansingModel/outputGenerator.h
@@ -34,7 +34,7 @@ void createOutputAgeDeath(const int t,
     /**This function creates output to research the average death age. **/
     if (t == 0) { // if time is at zero. Empty file before addition
         std::ofstream ofs;
-        ofs.open("outputDeathAge.csv"); // output file
+        ofs.open("outputDeathAge.txt"); // output file
         if (!ofs.is_open()){
             std::cerr << "Error. Unable to open output file.\n";
             exit(EXIT_FAILURE);
@@ -42,7 +42,7 @@ void createOutputAgeDeath(const int t,
         ofs.close();
     }
     std::ofstream ofs;
-    ofs.open("outputDeathAge.csv", std::ios::app); // output file for age of death
+    ofs.open("outputDeathAge.txt", std::ios::app); // output file for age of death
     ofs << t << " "
     << p.mutationProb << " "
     << p.extrinsicMortRisk << " "
@@ -58,7 +58,7 @@ void createOutputDeclineInGameteQuality(const int t,
                                         const std::vector<Individual>& deadIndividualsVec){
     if (t == 0) { // if time is at zero. Empty file before addition
         std::ofstream ofs;
-        ofs.open("outputDeclineGameteQuality.csv"); // output file
+        ofs.open("outputDeclineGameteQuality.txt"); // output file
         if (!ofs.is_open()){
             std::cerr << "Error. Unable to open output file.\n";
             exit(EXIT_FAILURE);
@@ -66,7 +66,7 @@ void createOutputDeclineInGameteQuality(const int t,
         ofs.close();
     }
     std::ofstream ofs;
-    ofs.open("outputDeclineGameteQuality.csv", std::ios::app); // output file for age of death
+    ofs.open("outputDeclineGameteQuality.txt", std::ios::app); // output file for age of death
     for (auto i : deadIndividualsVec){
         ofs << t << " "
         << i.age << " "
@@ -91,24 +91,79 @@ void createOutputLifeExpectancy(const Parameters& p,
     for (Individual male : males){
         double s = male.survivalProb * (1 - p.extrinsicMortRisk);
         double expectedAgeAtDeath = male.age + (s / (1 - s));
+        
+        // write maternal information
         ofs << male.age << " "
         << expectedAgeAtDeath << " "
         << male.ageOfMother << " "
+        << "F "
+        << male.survivalProb << " "
+        << p.mutationProbStemcell << " "
+        << p.mutationProb << std::endl;
+        
+        // write paternal information
+        ofs << male.age << " "
+        << expectedAgeAtDeath << " "
         << male.ageOfFather << " "
+        << "M "
         << male.survivalProb << " "
         << p.mutationProbStemcell << " "
         << p.mutationProb << std::endl;
     }
+
     
     for (Individual female : females){
         double s = female.survivalProb * (1 - p.extrinsicMortRisk);
         double expectedAgeAtDeath = female.age + (s / (1 - s));
+        
+        // write maternal information
         ofs << female.age << " "
         << expectedAgeAtDeath << " "
         << female.ageOfMother << " "
+        << "F "
+        << female.survivalProb << " "
+        << p.mutationProbStemcell << " "
+        << p.mutationProb << std::endl;
+        
+        // write paternal information
+        ofs << female.age << " "
+        << expectedAgeAtDeath << " "
         << female.ageOfFather << " "
+        << "M "
         << female.survivalProb << " "
         << p.mutationProbStemcell << " "
         << p.mutationProb << std::endl;
     }
+    ofs.close();
 }
+
+void createOutputTrackedIndividuals(const Parameters& p,
+                                    const indVec& trackedDeadIndividuals){
+    /**
+     Function to write the output of the tracked individuals. These are longitudinally followed by keeping track of the offspring they
+     get over their lifetime.
+     */
+    std::ofstream ofs;
+    ofs.open("outputLETrackedIndividuals.txt"); // the output file
+    if (!ofs.is_open()){
+        std::cerr << "Error. Unable to open output file.\n";
+        exit(EXIT_FAILURE);
+    }
+    for (size_t ind = 0; ind < trackedDeadIndividuals.size(); ++ind){ // loop through the flagged individuals
+        for (size_t i = 0; i < trackedDeadIndividuals[ind].offspringOfIndividual.size(); ++i){ // loop through the number of offspring this individual has
+            ofs << ind << " "; // use this index as identifier of the individual
+            
+            // calculate expected age at death for this offspring of the tracked individual
+            double s = trackedDeadIndividuals[ind].offspringOfIndividual[i].survivalProb * (1 - p.extrinsicMortRisk);
+            double expectedAgeAtDeath = trackedDeadIndividuals[ind].offspringOfIndividual[i].age + (s / (1 - s));
+            
+            // if this flagged individual is male, the age of the father needs to be documented, if female > age of mother will be documented
+            (trackedDeadIndividuals[ind].sex == 'M') ? ofs << trackedDeadIndividuals[ind].offspringOfIndividual[i].ageOfFather : ofs << trackedDeadIndividuals[ind].offspringOfIndividual[i].ageOfMother;
+            ofs << " "
+            << trackedDeadIndividuals[ind].offspringOfIndividual[i].survivalProb << " " // write survival probability to file
+            << expectedAgeAtDeath << std::endl; // write expected age at death to file
+        }
+    }
+    ofs.close();
+}
+
