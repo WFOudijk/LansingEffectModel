572608294a864b4c57ad53501e37baf7ca050ee5
WFOudijk <w.f.oudijk@student.rug.nl>
2023-03-30 13:39:19 +0200

    Update RScript gam analysis

--- LansingModel/LansingModel/rscripts/gam_analysis.R
+++ LansingModel/LansingModel/rscripts/gam_analysis.R
@@ -37,7 +37,7 @@ d <- d %>% mutate(y3 = car::logit(y2))
 
 # GAM analysis
 # remove some data otherwise it takes too long 
-d2 <- d %>% filter(na>6)
+d2 <- d %>% filter(na>20)
 
 ##########################
 #for damage-only scenario
@@ -55,7 +55,7 @@ summary(m1z)
 m1zb <- bam(y3 ~ s(ageOfParent, k = 5) + s(ageOfParent, ID, bs = "fs", k = 5) +
               s(mutationProbGametes, k = 5) +
               ti(ageOfParent, mutationProbGametes, k = c(5,5)) + 
-             # ti(ageOfParent, mutationProbSC, k = c(7,7)),
+              ti(ageOfParent, mutationProbSC, k = c(7,7)),
             #family=betar(link="logit"),
             data=d2,
             method = "REML")
@@ -63,6 +63,16 @@ summary(m1zb)
 ## ti effect significant (barely)
 # ti between Sc and age of parent significant, but ti between gamete and age of parent 
 # not ... Why? They mutate at the same rate. 
+
+# fit linear model to see if it is a better fit 
+# choose one mutation prob to perform both model analysis on 
+dat <- d2[d2$mutationProbGametes == 0.0004,]
+mlin <- lm(dat$y3 ~ dat$ageOfParent + dat$ID) # linear fit 
+m1zd <- bam(y3 ~ s(ageOfParent, k = 5) + s(ageOfParent, ID, bs = "fs", k = 5),
+            data = dat,
+            method = "REML") # bam analysis
+AIC(mlin, m1zd)
+# linear is better. Difference in AIC = 3.359.  
 
 AIC(m1z,m1zb)
 ## Appears to be an interaction since m1zb better
@@ -98,8 +108,8 @@ length(unique(d2$mutationProbSC)) #10
 # A single ID value because we will ignore the term involving ID anyway.
 d.pred <- expand.grid(ageOfParent=seq(0,40,length=len),
                       ID=levels(d2$ID)[1],
-                      mutationProbGametes=unique(d2$mutationProbGametes))
-                      #mutationProbSC=unique(d2$mutationProbSC))
+                      mutationProbGametes=unique(d2$mutationProbGametes),
+                      mutationProbSC=unique(d2$mutationProbSC))
 
 # Compute all terms, excluding the one with ID
 z <- predict(m1zb,newdata = d.pred,
@@ -114,15 +124,14 @@ logist <- function(x) 1/(1+exp(-x))
 d.pred$y4 <- 40*logist(d.pred$y3)
 
 # Use predictors as factors for use in ggplot
-d.pred <- d.pred %>% mutate(mutationProbGametes=factor(mutationProbGametes))
-                            #mutationProbSC=factor(mutationProbSC))
+d.pred <- d.pred %>% mutate(mutationProbGametes=factor(mutationProbGametes),
+                            mutationProbSC=factor(mutationProbSC))
 
 ggplot(d.pred,aes(x=ageOfParent,y=y4,color=mutationProbGametes)) +
   theme_cowplot() +
   geom_line() +
-  #facet_wrap(d.pred$mutationProbSC) +
+  facet_wrap(d.pred$mutationProbSC) +
   background_grid(major = "xy")
-
 
 
 ##########################
@@ -197,9 +206,14 @@ length(unique(d2$sdMutEffectSize)) #10
 # A single ID value because we will ignore the term involving ID anyway.
 d.pred <- expand.grid(ageOfParent=seq(0,40,length=len),
                       ID=levels(d2$ID)[1],
-                      mutationProbAgeGenes=unique(d2$mutationProbAgeGenes)[c(1,4,7,11)],
-                      meanMutBias=unique(d2$meanMutBias)[c(1,4,7,10)],
+                      mutationProbAgeGenes=unique(d2$mutationProbAgeGenes),
+                      meanMutBias=unique(d2$meanMutBias)[c(1,4,5,6)],
                       sdMutEffectSize=unique(d2$sdMutEffectSize)[c(1,4,7,10)])
+d.pred <- expand.grid(ageOfParent=seq(0,40,length=len),
+                      ID=levels(d2$ID)[1],
+                      mutationProbAgeGenes=unique(d2$mutationProbAgeGenes),
+                      meanMutBias=unique(d2$meanMutBias),
+                      sdMutEffectSize=unique(d2$sdMutEffectSize))
 
 # Compute all terms, excluding the one with ID
 z <- predict(m1zb,newdata = d.pred,
@@ -218,11 +232,39 @@ d.pred <- d.pred %>% mutate(mutationProbAgeGenes=factor(mutationProbAgeGenes),
                             meanMutBias=factor(meanMutBias),
                             sdMutEffectSize=factor(sdMutEffectSize))
 
+#library(cowplot)
 ggplot(d.pred,aes(x=ageOfParent,y=y4,color=mutationProbAgeGenes)) +
   theme_cowplot() +
   geom_line() +
   facet_wrap(meanMutBias~sdMutEffectSize) +
   background_grid(major = "xy")
+# the 'bump' is peculiar. However, the combination of parameters which show the bump is not present 
+# in original data. Might be explanation that model fits incorrectly. 
+# running the model with combination which shows bump. 
+# mean = -0.015; sd = 0.054; mutation rate age genes = 0.003
+dataTest <- read.table("/Users/willemijnoudijk/Documents/STUDY/Master Biology/ResearchProject1/data/CombiningDataForGam/outputLETrackedIndividuals.txt")
+dataTest <- read.table("/Users/willemijnoudijk/Documents/STUDY/Master Biology/ResearchProject1/data/CombiningDataForGam/outputLETrackedIndividualsTest.txt")
+colnames(dataTest) <- c("ID", "ageOfParent", "sexOfParent", "survivalProb", "expectedAgeAtDeath", "mutationProbGametes", "mutationProbSC", "mean", "sd", "mutationProbAgeGenes")
+
+# try smooth function from ggplot 
+ggplot(dataTest, aes(ageOfParent, expectedAgeAtDeath)) +
+  geom_smooth() 
+# no bump 
+
+d <- dataTest
+# perform d transformations as mentioned above 
+m1f <- bam(y3 ~ s(ageOfParent, k = 5) + s(ageOfParent, ID, bs = "fs", k = 5),
+           data = d2, 
+           method = "REML")
+d.pred.f <- expand.grid(ageOfParent = seq(0,40),
+                        ID = levels(d2$ID)[1])
+x <- predict(m1f, d.pred.f, type = "terms", 
+             exclude = c("s(ageOfParent,ID)"))
+d.pred.f$y3 <- attr(x, "constant") + apply(x, 1, sum)
+d.pred.f$y4 <- 40*logist(d.pred.f$y3)
+
+ggplot(d.pred.f, aes(ageOfParent, y4)) + geom_point() 
+# No bump. 
 
 # per parameter. 
 t <- subset(d.pred, d.pred$meanMutBias == -0.022 & d.pred$sdMutEffectSize == 0.024)
@@ -236,6 +278,13 @@ ggplot(t, aes(x = ageOfParent, y = y4, color = sdMutEffectSize)) +
   geom_line()
 # they resemble the ggplot geom_smooth a lot. 
 
+# number of parents per age class
+ggplot(d, aes(x = ageOfParent)) +
+  geom_density()
+
+# get averages
+averages <- aggregate(d$expectedAgeAtDeath, by = list(d$ageOfParent, d$mutationProbAgeGenes, d$meanMutBias, d$sdMutEffectSize), mean)
+mean(d[d$mutationProbAgeGenes == 0.0004 & d$meanMutBias == -0.015 & d$sdMutEffectSize == 0.054,]$expectedAgeAtDeath)
 
 #######################
 ## Predict, only using the first term (i.e. without the "random" effect)
@@ -263,36 +312,131 @@ plot(m1z, trans = logist,
 )
 
 ################################
-# try bayesian 
+# combining both model mechanisms
 ################################
+# first one more look at quality-only 
+# mean = - 0.022; sd = 0.024; mutation rate = 0.003
+quality_only <- read.table("/Users/willemijnoudijk/Documents/STUDY/Master Biology/ResearchProject1/data/CombiningDataForGam/outputLETrackedIndividualsOptParams.txt")
+colnames(quality_only) <- c("ID", "ageOfParent", "sexOfParent", "survivalProb", "expectedAgeAtDeath", "mutationProbGametes", "mutationProbSC", "mean", "sd", "mutationProbAgeGenes")
 
-library(brms)
-library(rstan)
-options(mc.cores = parallel::detectCores())
-rstan_options(auto_write = TRUE)
+d <- quality_only
 
-bf2 <- bf(y2 ~ s(ageOfParent) + s(ageOfParent, ID, bs = "fs", k = 5) +
-  s(mutationProbGametes, k = 5) +
-  t2(ageOfParent, mutationProbGametes, k = c(5,5))) +
-  zero_one_inflated_beta()
+# check number of parents.
+length(unique(d$ID))  
 
-my_priors <- prior(normal(0,1), class = Intercept) +
-  prior(normal(0,1), class = b)
+## Count how many unique ageOfParent per ID
+z <- d %>% group_by(ID) %>% summarize(na = length(unique(ageOfParent)))
+table(z$na) 
 
-m2 <- brm(bf2,
-          data = d,
-          prior = my_priors,
-          warmup = 1000,
-          iter = 2000,
-          chains = 4,
-          cores = 4,
-          backend = "cmdstanr",
-          control = list(adapt_delta = 0.99,
-                         max_treedepth = 10),
-          seed = 667,
-          file = "model1")
-### FATAL ERROR: a precompiled header has been changed. 
-# please rebuild precompiled header 'stan/src/stan/model/model_header.hpp.gch'
+## Add the counter to d
+d <- d %>% left_join(z,by="ID")
+
+## Filter out the one with 1 observation (but this selects ...)
+d <- d %>% filter(na>1)
+
+# change column ID into a factor 
+d <- d %>% mutate(ID = factor(ID)) 
+
+# compares 40 to the expected age at death. If expectedAgeAtDeath is lower > that will be y1; else it becomes 40 
+d <- d %>% mutate(y1 = pmin(40,expectedAgeAtDeath)) 
+# scale the expected age at deaths to the maximum age. 
+d <- d %>% mutate(y2 = y1/40)
+## Work with logits (then (0,1) -> (-inf,+inf))
+d <- d %>% mutate(y3 = car::logit(y2))
+
+# GAM analysis
+# remove some data otherwise it takes too long 
+d2 <- d %>% filter(na>11)
+
+m1a <- bam(y3 ~ s(ageOfParent, k = 5) +
+             s(ageOfParent, ID, bs = "fs", k = 5 ),
+           data = d2, 
+           method = "REML")
+summary(m1a)
+logist <- function(x) 40/(1+exp(-x))
+
+d2 <- d2 %>% mutate(ID = factor(ID)) 
+
+pred_data = expand.grid(ageOfParent = c(0,40), ID = levels(d2$ID)[1])
+
+## Predict, only using the first term (i.e. without the "random" effect)
+z <- predict(m1a, newdata = pred_data, type="terms",terms = c("s(ageOfParent)"))
+z
+str(z)
+## Use logist transformed:
+zl <- logist(z)
+
+## Percentage decrease:
+perc_decr <- 100*(zl[1]-zl[2])/zl[1]
+perc_decr
+
+plot(m1a, trans = logist,
+     main = paste("Quality-only, % decrease =", perc_decr),
+     subtitle = "Mutation rate = 0.003; mean = -0.022; sd = 0.024",
+     xlab = "Age of parent",
+     ylab = "Expected age at death")
+
+# look at damage-only with gametes = stem cells = 0.0024
+damage_only <- read.table("/Users/willemijnoudijk/Documents/STUDY/Master Biology/ResearchProject1/data/CombiningDataForGam/outputLETrackedIndividualsOptParamsDamage.txt")
+colnames(damage_only) <- c("ID", "ageOfParent", "sexOfParent", "survivalProb", "expectedAgeAtDeath", "mutationProbGametes", "mutationProbSC", "mean", "sd", "mutationProbAgeGenes")
+
+d <- damage_only
+
+# check number of parents.
+length(unique(d$ID))  
+
+## Count how many unique ageOfParent per ID
+z <- d %>% group_by(ID) %>% summarize(na = length(unique(ageOfParent)))
+table(z$na) 
+
+## Add the counter to d
+d <- d %>% left_join(z,by="ID")
+
+## Filter out the one with 1 observation (but this selects ...)
+d <- d %>% filter(na>1)
+
+# change column ID into a factor 
+d <- d %>% mutate(ID = factor(ID)) 
+
+# compares 40 to the expected age at death. If expectedAgeAtDeath is lower > that will be y1; else it becomes 40 
+d <- d %>% mutate(y1 = pmin(40,expectedAgeAtDeath)) 
+# scale the expected age at deaths to the maximum age. 
+d <- d %>% mutate(y2 = y1/40)
+## Work with logits (then (0,1) -> (-inf,+inf))
+d <- d %>% mutate(y3 = car::logit(y2))
+
+# GAM analysis
+# remove some data otherwise it takes too long 
+d2 <- d %>% filter(na>11)
+
+m1a <- bam(y3 ~ s(ageOfParent, k = 5) +
+             s(ageOfParent, ID, bs = "fs", k = 5 ),
+           data = d2, 
+           method = "REML")
+summary(m1a)
+logist <- function(x) 40/(1+exp(-x))
+
+d2 <- d2 %>% mutate(ID = factor(ID)) 
+
+pred_data = expand.grid(ageOfParent = c(0,40), ID = levels(d2$ID)[1])
+
+## Predict, only using the first term (i.e. without the "random" effect)
+z <- predict(m1a, newdata = pred_data, type="terms",terms = c("s(ageOfParent)"))
+z
+str(z)
+## Use logist transformed:
+zl <- logist(z)
+
+## Percentage decrease:
+perc_decr <- 100*(zl[1]-zl[2])/zl[1]
+perc_decr
+
+plot(m1a, trans = logist,
+     main = paste("Quality-only, % decrease =", perc_decr),
+     subtitle = "Mutation rate = 0.003; mean = -0.022; sd = 0.024",
+     xlab = "Age of parent",
+     ylab = "Expected age at death")
+
 
 
 
