907a3b8587568b8319036035a6439b787e1d104a
WFOudijk <w.f.oudijk@student.rug.nl>
2023-04-20 14:58:13 +0200

    Update params

--- LansingModel/LansingModel/parameters.h
+++ LansingModel/LansingModel/parameters.h
@@ -35,9 +35,9 @@ struct Parameters {
                    pointOfHalfScalingParam(0.5),
                    baselineSurvival(0.5),
                    scalingStrengthOfAllocationToSurvival(0.2),
-                   addBinary(true),
+                   addBinary(false),
                    addAgeSpecific(false),
-                   addQuality(false),
+                   addQuality(true),
                    addInvestmentInRepair(false),
                    addInvestmentAffectingOffspringQuality(true){
                        numOfGametes = maximumAge * numOfOffspringPerFemale;
