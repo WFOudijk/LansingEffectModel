4e4cb97d99a9b3c6dc3de7025f9979d219265a50
WFOudijk <w.f.oudijk@student.rug.nl>
2023-04-26 13:35:36 +0200

    update params

--- LansingModel/LansingModel/parameters.h
+++ LansingModel/LansingModel/parameters.h
@@ -35,7 +35,7 @@ struct Parameters {
                    pointOfHalfScalingParam(0.5),
                    baselineSurvival(0.5),
                    scalingStrengthOfAllocationToSurvival(0.2),
-                   addBinary(false),
+                   addBinary(true),
                    addAgeSpecific(true),
                    addQuality(false),
                    addInvestmentInRepair(false),
