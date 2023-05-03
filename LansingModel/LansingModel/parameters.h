06852c348678c41015aa950aa91d26d8e9a92ed7
WFOudijk <w.f.oudijk@student.rug.nl>
2023-05-03 15:30:37 +0200

    update params for long simulation

--- LansingModel/LansingModel/parameters.h
+++ LansingModel/LansingModel/parameters.h
@@ -15,7 +15,7 @@ struct Parameters {
                    mutationProb(0.0024),
                    extrinsicMortRisk(0.05), // maximum added number of years to live will be 19
                    outputTime(10),
-                   tEnd(100000), // 10.000
+                   tEnd(500000), // 10.000
                    strengthOfSelection(-0.05),
                    maximumAge(40),
                    mutationProbStemcell(0.0024),
@@ -36,10 +36,10 @@ struct Parameters {
                    baselineSurvival(0.5),
                    scalingStrengthOfAllocationToSurvival(0.2),
                    addBinary(false),
-                   addAgeSpecific(false),
+                   addAgeSpecific(true),
                    addQuality(false),
                    addInvestmentInRepair(false),
-                   addInvestmentAffectingOffspringQuality(true){
+                   addInvestmentAffectingOffspringQuality(false){
                        numOfGametes = maximumAge * numOfOffspringPerFemale;
                    }
     
